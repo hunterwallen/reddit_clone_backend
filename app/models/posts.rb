18e21e9c09e77b1f39109a48435bbf3b18d17419
Evan Epperson <evanepperson@pop-os.localdomain>
2021-01-28 12:10:27 -0600

    saving half way through posts.rb

--- app/models/posts.rb
+++ app/models/posts.rb
@@ -2,6 +2,36 @@ class Post
     DB = PG.connect({host: '', port: 5432, dbname: 'reddit_development', password: 'hello'})
     
     def self.all
+        results = DB.exec("SELECT * FROM posts;")
+        return results.each do |result|
+            {
+                "id" => result["id"].to_i,
+                "title" => result["title"],
+                "body" => result["body"],
+                "user" => result["user"]
+            }
+        end
+    end
+
+    def self.find(id)
+        results = DB.exec("SELECT * FROM posts WHERE id=#{id};")
+        return 
+        { 
+            "id" => results.first["id"].to_i,
+            "title" => results.first["title"],
+            "body" => results.first["body"],
+            "user" => results.first["user"]
+        }
+    end
+
+    def self.create(opts)
+        results = DB.exec(
+            <<-SQL
+                    INSERT INTO posts (title, body, user)
+                    VALUES ('#{opts["title"]}', '#{opts["body"]', '#{opts["user"]')
+                    RETURNING id, title, body, user
+            SQL
+        )
         
     end
 
