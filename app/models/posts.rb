16647846edfcc7a764fe448dafd416247616b0f4
Evan Epperson <evanepperson@pop-os.localdomain>
2021-01-28 12:24:46 -0600

    finishing posts.rb

--- app/models/posts.rb
+++ app/models/posts.rb
@@ -6,9 +6,9 @@ class Post
         return results.each do |result|
             {
                 "id" => result["id"].to_i,
+                "user" => result["user"],
                 "title" => result["title"],
-                "body" => result["body"],
-                "user" => result["user"]
+                "body" => result["body"]
             }
         end
     end
@@ -18,9 +18,9 @@ class Post
         return 
         { 
             "id" => results.first["id"].to_i,
+            "user" => results.first["user"],
             "title" => results.first["title"],
-            "body" => results.first["body"],
-            "user" => results.first["user"]
+            "body" => results.first["body"]
         }
     end
 
@@ -28,10 +28,38 @@ class Post
         results = DB.exec(
             <<-SQL
                     INSERT INTO posts (title, body, user)
-                    VALUES ('#{opts["title"]}', '#{opts["body"]', '#{opts["user"]')
+                    VALUES ('#{opts["user"]}', '#{opts["title"]', '#{opts["body"]')
                     RETURNING id, title, body, user
             SQL
         )
+        return {
+            "id" => results.first["id"].to_i,
+            "user" => results.first["user"],
+            "title" => results.first["title"],
+            "body" => results.first["body"]       
+        }
+    end
+
+    def self.delete(id)
+        results = DB.exec("DELETE FROM posts WHERE id=#{id}")
+        return {"deleted" => true}
+    end
+
+    def self.update(id, opts)
+        results = DB.exec(
+            <<-SQL
+                UPDATE posts
+                SET user='#{opts["user"]}', title='#{opts["title"]', body='#{opts["body"]'
+                WHERE id=#{id}
+                RETURNING id, user, title, body;
+            SQL
+        )
+        return{
+            "id" => results.first["id"].to_i,
+            "user" => results.first["user"],
+            "title" => results.first["title"],
+            "body" => results.first["body"]    
+        }
         
     end
 
