cbc28f10c7b11cbc5fc44ecfaa142a6d77166d07
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-28 15:45:13 -0700

    fixed post.rb bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,50 +1,11 @@
-class Post
+class Post < ApplicationRecord
     DB = PG.connect({host: 'localhost', port: 5432, dbname: 'reddit_development'})
-    #dont forget to add password and turn local host to empty string Evan
-    def self.all
-        results = DB.exec("SELECT * FROM posts;")
-        return results.each do |result|
-            {
-                "id" => result["id"].to_i,
-                "author" => result["author"],
-                "title" => result["title"],
-                "body" => result["body"]
-            }
-        end
-    end
-
-    def self.find(id)
-        results = DB.exec("SELECT * FROM posts WHERE id=#{id};")
-        return
-        {
-            "id" => results.first["id"].to_i,
-            "author" => results.first["author"],
-            "title" => results.first["title"],
-            "body" => results.first["body"]
-        }
-    end
-
-    def self.create(opts)
-        results = DB.exec(
-            <<-SQL
-            INSERT INTO posts(author, title, body)
-            VALUES ('#{opts["author"]}', '#{opts["title"]}', '#{opts["body"]}')
-            RETURNING *;
-            SQL
-        )
-        return {
-            "id" => results.first["id"].to_i,
-            "author" => results.first["author"],
-            "title" => results.first["title"],
-            "body" => results.first["body"]
-        }
-    end
 
     def self.delete(id)
         results = DB.exec("DELETE FROM posts WHERE id=#{id}")
         return {"deleted" => true}
     end
-
+    #
     def self.update(id, opts)
         results = DB.exec(
             <<-SQL
