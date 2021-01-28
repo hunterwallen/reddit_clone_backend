482d2698e423d0d256daf3b4c3741594c6bb2f6c
Kim Blanck <kimblanck@kims-air.home>
2021-01-28 13:11:29 -0500

    schema and seeds files

--- db/migrate/20210128175405_create_post.rb
+++ db/migrate/20210128175405_create_post.rb
@@ -1,7 +1,9 @@
 class CreatePost < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.string :user
+      t.string :title
+      t.string :body
       t.timestamps
     end
   end
