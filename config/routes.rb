c834c33c432311e2c7264c7063a0a3748d64581c
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-28 18:04:32 -0700

    fixed bug in post.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   put '/posts/:id', to: 'posts#update'
 
   delete '/posts/:id', to: 'posts#delete'
+
+  post '/postsseed', to: 'posts#create_many'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
