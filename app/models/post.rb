9db0a88735edf6bd9b090c5259b29aa13cfddeb6
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-29 16:55:55 -0700

    error hutning

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ApplicationRecord
 
-    if ENV["DATABASE_URL"]
-      PG.connect(ENV['DATABASE_URL'])
-    elsif
-      DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'reddit_development'})
-    end
+  if(ENV['DATABASE_URL'])
+  uri = URI.parse(ENV['DATABASE_URL'])
+  DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+  else
+  DB = PG.connect(host: "localhost", port: 5432, dbname: 'reddit_development')
+  end
 
 
     def self.all
