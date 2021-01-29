7a6779accf5d92fb485eaa89a1c5a6779117b02b
Evan Epperson <evanepperson@pop-os.localdomain>
2021-01-29 16:56:19 -0600

    saving bcrypt user

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,26 @@ Rails.application.routes.draw do
   delete '/posts/:id', to: 'posts#delete'
 
   post '/postsseed', to: 'posts#create_many'
+
+
+
+
+
+
+
+
+  
+
+  get '/accounts', to: 'accounts#index'
+
+  get '/accounts/:id', to: 'accounts#show'
+
+  post '/accounts', to: 'accounts#create'
+
+  put '/accounts/:id', to: 'accounts#update'
+
+  delete '/accounts/:id', to: 'accounts#delete'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
