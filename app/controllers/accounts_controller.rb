d261b158081bf2b473d5ea918276c04674a7739c
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-29 16:33:20 -0700

    bcrypt linked

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,28 +1,28 @@
-class AccountsController < ApplicationController
-
-  def index
-    render json: Account.all
-  end
-
-  # def show
-  #   render json: Account.find(params["id"])
-  # end
-
-  def create
-    render json: Account.create(params["account"])
-  end
-
-  # def update
-  #   render json: Account.update(params["id"], params["account"])
-  # end
-
-  # def delete
-  #   render json: Account.delete(params["id"])
-  # end
-
-
-
-
-
-
-end
+# class AccountsController < ApplicationController
+#
+#   def index
+#     render json: Account.all
+#   end
+#
+#   # def show
+#   #   render json: Account.find(params["id"])
+#   # end
+#
+#   def create
+#     render json: Account.create(params["account"])
+#   end
+#
+#   # def update
+#   #   render json: Account.update(params["id"], params["account"])
+#   # end
+#
+#   # def delete
+#   #   render json: Account.delete(params["id"])
+#   # end
+#
+#
+#
+#
+#
+#
+# end
